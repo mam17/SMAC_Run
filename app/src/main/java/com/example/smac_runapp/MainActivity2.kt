dec8b3678371e9719800e8c256c58c8b9303a91a
Longg <duongthanhlong1710@gmail.com>
2022-11-08 11:49:56 +0700

    aaaaa

--- app/src/main/java/com/example/smac_runapp/MainActivity2.kt
+++ app/src/main/java/com/example/smac_runapp/MainActivity2.kt
@@ -37,7 +37,7 @@ enum class FitActionRequestCode {
     SUBSCRIBE,
     READ_DATA
 }
-//tôi là long
+//tôi là long ahuhdcosahidsaas
 class MainActivity2 : AppCompatActivity() {
     private val fitnessOptions = FitnessOptions.builder()
         .addDataType(DataType.TYPE_STEP_COUNT_CUMULATIVE)
